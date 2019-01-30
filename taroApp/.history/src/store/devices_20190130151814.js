4ae64ca3c603d5ab95c33cf956c4cd87abe42eb6
gaoyingqiao <gaoyinqiao@sluan.com>
2019-01-30 15:40:11 +0800

    delete history folder

--- taroApp/.history/src/store/devices_20190130151814.js
+++
@@ -1,8 +0,0 @@
-import { observable } from 'mobx'
-
-const devicesStore = observable({
-  devices: [],
-  sort: 'desc', // 创建时间排序 desc/asc
-})
-
-export default devicesStore
--- taroApp/.history/src/store/devices_20190130152229.js
+++
@@ -1,8 +0,0 @@
-import { observable } from 'mobx'
-
-class devicesStore {
-  @observable devices = []
-  @observable sort = 'desc' // 创建时间排序 desc/asc
-}
-
-export default new devicesStore()
